a4c9af75a8fa4c61ea3c94852a7730bb0e329b0d
sara-pulido <sara01rizo@gmail.com>
2022-02-16 22:57:58 +0100

    Add: goals controllers and routes set up - pkg json

--- backend/controllers/goalsControllers.js
+++ backend/controllers/goalsControllers.js
@@ -1,14 +1,16 @@
+const asyncHandler = require('express-async-handler')
+
 // desc     Get goals
 // route    GET /api/goals
 // access   Private               
-const getGoals = (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
     res.status(200).json({ message: 'Get goals'})
-}
+})
 
 // desc     Set goal
 // route    POST /api/goals
 // access   Private               
-const setGoal = (req, res) => {
+const setGoal = asyncHandler(async (req, res) => {
     if (!req.body.text) { 
         res.status(400)
         throw new Error('Please add a text field')
@@ -16,21 +18,21 @@ const setGoal = (req, res) => {
 }
 
     res.status(200).json({ mesagge: 'Set goal'})
-}
+})
 
 // desc     Update goal
 // route    PUT /api/goals/:id
 // access   Private               
-const updateGoal = (req, res) => {
+const updateGoal = asyncHandler(async (req, res) => {
     res.status(200).json({ mesagge: `Update goal ${req.params.id}`})
-}
+})
 
 // desc     Delete goal
 // route    Delete /api/goals/:id
 // access   Private               
-const deleteGoal = (req, res) => {
+const deleteGoal = asyncHandler(async (req, res) => {
     res.status(200).json({ mesagge: `Delete goal ${req.params.id}`})
-}
+})
 
 module.exports = {
     getGoals,
