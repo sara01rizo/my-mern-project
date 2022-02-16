d9be5070ad72f4fcfc23063b5bc7fd37dc8a2d60
sara-pulido <sara01rizo@gmail.com>
2022-02-16 22:05:11 +0100

    SET: my goals controllers for all the routes

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,21 +1,14 @@
 const express = require('express')
 const router = express.Router()
+const { getGoals, setGoal, updateGoal, deleteGoal } = require('../controllers/goalsControllers')
 
-router.get('/', (req, res) => {
-    res.status(200).json({ mesagge: 'Get goals'})
-})
+router.get('/', getGoals)
 
-router.post('/', (req, res) => {
-    res.status(200).json({ mesagge: 'Set goal'})
-})
+router.post('/', setGoal)
 
-router.put('/:id', (req, res) => {
-    res.status(200).json({ mesagge: `Update goal ${req.params.id}`})
-})
+router.put('/:id', updateGoal)
 
-router.delete('/:id', (req, res) => {
-    res.status(200).json({ mesagge: `Delete goal ${req.params.id}`})
-})
+router.delete('/:id', deleteGoal)
 
 module.exports = router
 
