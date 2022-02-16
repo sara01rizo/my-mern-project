046e6044f9b9c7bbcf2da27e75b7161454fa83af
sara-pulido <sara01rizo@gmail.com>
2022-02-16 21:27:44 +0100

    Update: confirm with Postman GET works

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 const express = require('express')
+const { env } = require('process')
 const dotenv = require('dotenv').config()
-const port = 5000
+const port = process.my-env.PORT || 5000
 
 const app = express()
+
+app.get('/api/goals', (req, res) => {
+    res.json({ mesagge: 'Get goals'})
+})
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
