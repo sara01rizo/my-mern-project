027486bda4b9172a640b63aae9d5efe73bb69ac6
sara-pulido <sara01rizo@gmail.com>
2022-02-16 22:11:40 +0100

    CLEAN: goalRoutes file

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,14 +1,10 @@
 const express = require('express')
+const { get } = require('http')
 const router = express.Router()
 const { getGoals, setGoal, updateGoal, deleteGoal } = require('../controllers/goalsControllers')
 
-router.get('/', getGoals)
-
-router.post('/', setGoal)
-
-router.put('/:id', updateGoal)
-
-router.delete('/:id', deleteGoal)
+router.route('/').get(getGoals).post(setGoal)
+router.route('/:id').delete(deleteGoal).put(updateGoal)
 
 module.exports = router
 
