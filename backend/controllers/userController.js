823d1f2fbd6266011dc935460d287932a20cada6
sara-pulido <sara01rizo@gmail.com>
2022-02-18 23:31:08 +0100

    Create: set up user authentication - incluid bycryp compare user password

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -49,7 +49,21 @@ const registerUser = asyncHnadler(async (req, res) => {
 // route    POST /api/users/login
 // access   Public 
 const loginUser = asyncHnadler(async (req, res) => {
-    res.json({ message: 'Login User'})
+    const {email, password} = req.body
+
+    //Check for the user email
+    const user = await User.findOne({email})
+
+    if(user && (await bcrypt.compare(password, user.password))) {
+       res.json({
+           _id: user.id,
+           name: user.name,
+           email: user.email,
+       }) 
+    } else {
+        res.status(400)
+        throw new Error('Invalid logins')
+    }
 })
 
 // desc     Get User data
