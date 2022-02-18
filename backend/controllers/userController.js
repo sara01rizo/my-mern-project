90a8e5e0cc76d349aa7eaa412a3736bc4ebe2c04
sara-pulido <sara01rizo@gmail.com>
2022-02-18 23:21:14 +0100

    Create: user and password - user method findOne - incluid throw new Error

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,14 +7,42 @@ const User = require('../models/userModel')
 // route    POST /api/users
 // access   Public 
 const registerUser = asyncHnadler(async (req, res) => {
-    const { name, email, passwors } = req.body
+    const { name, email, password } = req.body
 
     if(!name || !email || !password) {
         res.status(400)
         throw new Error('Please add all fields')
     }
 
-    res.json({ message: 'Register User'})
+    //Check if user exists
+    const userExist = await User.findOne({email})
+
+    if(userExist) {
+        res.status(400)
+        throw new Error('User already exist')
+    }
+
+    // Hash password 
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password)
+
+    // Create user
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword
+    })
+
+    if(user) {
+        res.status(201).json({
+            _id: user.id,
+            name: user.name,
+            email: user.email
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
 })
 
 // desc     Authenticate a User
