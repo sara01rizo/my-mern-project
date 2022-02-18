fc2c8244395e9469407a3a7081b13872953372fc
sara-pulido <sara01rizo@gmail.com>
2022-02-18 22:49:50 +0100

    Add: set up user Routes for register- log in and get Me

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,24 @@
+// desc     Register new User
+// route    POST /api/users
+// access   Public 
 const registerUser = (req, res) => {
     res.json({ message: 'Register User'})
 } 
 
+// desc     Authenticate a User
+// route    POST /api/users/login
+// access   Public 
+const loginUser = (req, res) => {
+    res.json({ message: 'Login User'})
+} 
+
+// desc     Get User data
+// route    GET /api/users/me
+// access   Public 
+const getMe = (req, res) => {
+    res.json({ message: 'User display info'})
+} 
+
 module.exports = {
-    registerUser,
+    registerUser, loginUser, getMe
 }
