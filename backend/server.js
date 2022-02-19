cd3d5a9f62f2b3fa3ac6191edea97457b9379083
sara-pulido <sara01rizo@gmail.com>
2022-02-19 03:14:04 +0100

    Add: install cors -server root- to solve error and connect backend-frontend

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,25 @@ const dotenv = require('dotenv').config()
 const colors = require('colors')
 const {errorHandler} = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
+const cors = require('cors')
 const port = process.my-env.PORT || 5000
 
 connectDB()
 
 const app = express()
+
+const whitelist = ["http://localhost:3000"]
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error("Not allowed by CORS"))
+    }
+  },
+  credentials: true,
+}
+app.use(cors(corsOptions))
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
