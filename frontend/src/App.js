da7e47bb7caaf904b3791af1f10af5427cb27fd0
sara-pulido <sara01rizo@gmail.com>
2022-02-22 23:00:58 +0100

    Add: react-router-dom in this version use Routes and after path use element

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
-    <div className="App">
-      <h1>My MERN App template</h1>
+    <>
+    <Router>
+    <div className="container">
+      <Routes>
+        <Route path='/' element={<Dashboard />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+      </Routes>
     </div>
+    </Router>
+    </>
   );
 }
 
