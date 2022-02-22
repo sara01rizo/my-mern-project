0c943048f95d25afd903a4a4a7ba368f35ce9444
sara-pulido <sara01rizo@gmail.com>
2022-02-23 00:52:12 +0100

    Debugg: crbug/1173575 and update: authMiddleware file

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,35 +1,36 @@
 const jwt = require('jsonwebtoken')
-const { eventNames } = require('../models/userModel')
-const asyncHnadler = ('express-async-handler')
+const asyncHandler = require('express-async-handler')
 const User = require('../models/userModel')
 
-const protect = asyncHnadler(async (req, res, next) => {
-    let token
+const protect = asyncHandler(async (req, res, next) => {
+  let token
 
-    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
-        try {
-            //Get token from header
-            // 1 bc we want the one index
-            token = req.headers.authorization.split('')[1]
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    try {
+      // Get token from header
+      token = req.headers.authorization.split(' ')[1]
 
-            //let's verify the token
-            const decoded = jwt.verify(token, process.my-env.JWT_SECRET)
+      // Verify token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
-            // Get user from the token
-            req.user = await User.findById(decoded.id).select('-password')
+      // Get user from the token
+      req.user = await User.findById(decoded.id).select('-password')
 
-            next()
-        } catch (error) {
-            console.log(error)
-            res.status(401)
-            throw new Error('Sorry, not authorized')
-        }
+      next()
+    } catch (error) {
+      console.log(error)
+      res.status(401)
+      throw new Error('Not authorized')
     }
-    if(!token){
-        res.status(401)
-        throw new Error('Not authorized, sorry no token for you')
-    }
+  }
 
+  if (!token) {
+    res.status(401)
+    throw new Error('Not authorized, no token')
+  }
 })
 
 module.exports = { protect }
