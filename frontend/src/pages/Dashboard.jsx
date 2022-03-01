a189d50c91fb2c694ed4ea0ff69a41cff85bf331
sara-pulido <sara01rizo@gmail.com>
2022-03-01 02:15:53 +0100

    Fix and clean: Dashboard jsx file, console clean

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,24 +1,26 @@
 import { useEffect } from 'react'
-import {useNavigate} from 'react-router-dom'
-import {useSelector, useDispatch} from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
 import GoalForm from '../components/GoalForm'
 import Spinner from '../components/Spinner'
 import GoalItem from '../components/GoalItem'
-import {getGoals, reset} from '../features/goals/goalSlice'
+import { getGoals, reset } from '../features/goals/goalSlice'
 
 function Dashboard() {
   const navigate = useNavigate()
   const dispatch = useDispatch()
 
   const { user } = useSelector((state) => state.auth)
-  const { goals, isLoading, isError, message } = useSelector((state) => state.goals)
+  const { goals, isLoading, isError, message } = useSelector(
+    (state) => state.goals
+  )
 
   useEffect(() => {
-    if(isError) {
-      console.log(message);
+    if (isError) {
+      console.log(message)
     }
 
-    if(!user) {
+    if (!user) {
       navigate('/login')
     }
 
@@ -35,23 +37,24 @@ function Dashboard() {
 
   return (
     <>
-    <section className='heading'>
-      <h1>Welcome {user && user.name} </h1>
-      <p>Goals Dashboard</p>
-    </section>
+      <section className='heading'>
+        <h1>Welcome {user && user.name}</h1>
+        <p>Goals Dashboard</p>
+      </section>
 
-    <GoalForm />
+      <GoalForm />
 
-    <section className="content">
-      {goals.length > 0 ? (
-        <div className="div-goals">
-          {goals.map((goal) => (
-            <GoalItem key={goal._id} goal={goal} />
-          ))}
-        </div>
-      ) : (<h3>You have no set any Goals</h3>)}
-    </section>
-
+      <section className='content'>
+        {goals.length > 0 ? (
+          <div className='goals'>
+            {goals.map((goal) => (
+              <GoalItem key={goal._id} goal={goal} />
+            ))}
+          </div>
+        ) : (
+          <h3>You have not set any goals</h3>
+        )}
+      </section>
     </>
   )
 }
